85c2be8b819e21730fd8c4f36ec21208cbbe71f6
Shamberger Michael Ext <ext-michael.shamberger@hus.fi>
2019-06-17 13:23:51 +0300

    add batch file

---
+++ python/run.bat
@@ -0,0 +1,15 @@
+
+set SPARK_HOME = C:\Users\ext-shambmi\spark-2.4.3-bin-hadoop2.7
+set HADOOP_HOME = C:\Users\ext-shambmi\winutils
+set PATH += C:\Users\ext-shambmi\spark-2.4.3-bin-hadoop2.7\bin
+set PATH += C:\ProgramData\Anaconda3\
+
+
+rem export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH
+rem export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.10.3-src.zip:$PYTHONPATH
+
+rem remove the old datacohort files
+rem rm -r -f ../datacohort/*
+
+rem run main
+python ./src/SurgeonScorecard.py
