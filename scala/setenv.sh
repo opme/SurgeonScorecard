17c3c882b1480a26d90853fda5cf10e731e0e6af
Shamberger Michael Ext <ext-michael.shamberger@hus.fi>
2019-06-25 14:24:19 +0300

    add setenv

---
+++ scala/setenv.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export CLASSPATH=$CLASSPATH:/c/users/ext-shambmi/spark-2.4.3-bin-hadoop2.7/jars
+export JAVA_OPTS="$JAVA_OPTS -Dhttp.proxyHost=husproxy.hus.fi -Dhttp.proxyPort=8080 -Dhttps.proxyHost=husproxy.hus.fi -Dhttps.proxyPort=8080" 
